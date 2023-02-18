03e4d6718e745139a1fac7fca1d023505e732b10
Jay Bambhroliya <jaybambharoliya1084@gmail.com>
2023-02-18 10:41:47 +0530

    adding some code related to list in python

---
+++ Working_with_List/Animals.py
@@ -0,0 +1,5 @@
+animals = ['cat', 'dog', 'horse']
+for animal in animals:
+    print(animal.title())
+    print(f"A {animal.title()} would make a great pet\n")
+print("Any of these animals would make a great pet")
